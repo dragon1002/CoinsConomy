46ebb75ce32e447a706431b457fe5a53343685af
mailoplatform <jannik.mueller.2000@gmail.com>
2014-05-11 18:22:03 +0200

    Plugin

---
+++ src/eu/mchub/cc/main/main.java
@@ -0,0 +1,10 @@
+package eu.mchub.cc.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+	public void onEnable() {
+		System.out.println("Test wird gestartet");
+	}
+}
